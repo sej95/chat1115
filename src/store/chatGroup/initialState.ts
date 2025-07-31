16b0e57f4a79a69a3d3431008653a5bd4ab30fcf
rivertwilight <wrj2014@126.com>
2025-07-31 15:18:17 +0800

    feat: Group settings panel

--- src/store/chatGroup/initialState.ts
+++ src/store/chatGroup/initialState.ts
@@ -3,9 +3,11 @@ import { ChatGroupItem } from '@/types/chatGroup';
 export interface ChatGroupState {
   groups: ChatGroupItem[];
   isGroupsLoading: boolean;
+  showGroupSetting: boolean;
 }
 
 export const initialChatGroupState: ChatGroupState = {
   groups: [],
   isGroupsLoading: true,
+  showGroupSetting: false,
 }; 
